a85503e38482d85608d16c6ebdc2595b9e51a463
gwind <lijian@ooclab.com>
2016-11-03 22:34:21 +0800

    specify tunnel config by command line in simple client of example

---
+++ example/README.md
@@ -0,0 +1,19 @@
+build:
+
+```
+go build -v -race server.go
+go build -v -race client.go
+```
+
+run server:
+
+```
+./server :3001
+```
+
+run client:
+
+```
+./client 127.0.0.1:3001
+```
+
--- example/client.go
+++ example/client.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 
@@ -11,8 +14,9 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage: %s Address\n", os.Args[0])
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s Address Tunnel\n", os.Args[0])
+		fmt.Printf("Example: %s 127.0.0.1:3000 f:127.0.0.1:10080:127.0.0.1:8080", os.Args[0])
 		os.Exit(1)
 	}
 
@@ -26,12 +30,47 @@ func main() {
 	l := link.NewLink(nil)
 	l.Bind(conn)
 
-	localHost := "127.0.0.1"
-	localPort := 10080
-	remoteHost := "127.0.0.1"
-	remotePort := 3000
-	reverse := false
+	// localHost := "127.0.0.1"
+	// localPort := 10080
+	// remoteHost := "127.0.0.1"
+	// remotePort := 3000
+	// reverse := false
+	localHost, localPort, remoteHost, remotePort, reverse, err := parseTunnel(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
 	l.OpenTunnel(localHost, localPort, remoteHost, remotePort, reverse)
 
 	l.WaitDisconnected()
+}
+
+func parseTunnel(value string) (localHost string, localPort int, remoteHost string, remotePort int, reverse bool, err error) {
+	L := strings.Split(value, ":")
+	if len(L) != 5 {
+		fmt.Println("tunnel format: \"r|f:local_host:local_port:remote_host:remote_port\"")
+		err = errors.New("tunnel map is wrong: " + value)
+		return
+	}
+
+	localHost = L[1]
+	remoteHost = L[3]
+	switch L[0] {
+	case "r", "R":
+		reverse = true
+	case "f", "F":
+		reverse = false
+	default:
+		err = errors.New("wrong tunnel map")
+		return
+	}
+	localPort, err = strconv.Atoi(L[2])
+	if err != nil {
+		return
+	}
+	remotePort, err = strconv.Atoi(L[4])
+	if err != nil {
+		return
+	}
+
+	return
 }
