48cee86e62ea8cb3e8af3c51b10ab8a67e450d7e
gwind <lijian@ooclab.com>
2017-01-10 16:48:09 +0800

    add udp example

---
+++ example/udp/client.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/ooclab/es/proto/udp"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s RemoteAddress\n", os.Args[0])
+		return
+	}
+	// laddr, err := net.ResolveUDPAddr("udp", os.Args[1])
+	// if err != nil {
+	// 	logrus.Errorf("resolve local udp addr %s failed: %s", os.Args[1], err)
+	// 	return
+	// }
+
+	raddr, err := net.ResolveUDPAddr("udp", os.Args[1])
+	if err != nil {
+		logrus.Errorf("resolve remote udp addr %s failed: %s", os.Args[2], err)
+		return
+	}
+	conn, err := net.ListenUDP("udp", nil)
+	// conn, err := net.DialUDP("udp", nil, raddr)
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+
+	sock, clientConn, err := udp.NewClientSocket(conn, raddr)
+	fmt.Println("sock = ", sock)
+
+	maxSize := 1024 * 1024
+	for i := 2; i <= maxSize; i = i * 2 {
+		b := make([]byte, i)
+		rand.Read(b)
+		sc := md5.Sum(b)
+		if err := clientConn.SendMsg(b); err != nil {
+			logrus.Errorf("SendMsg failed: %s", err)
+			break
+		}
+		fmt.Printf("%9d --> send %s\n", len(b), hex.EncodeToString(sc[:]))
+		msg, err := clientConn.RecvMsg()
+		if err != nil {
+			logrus.Errorf("RecvMsg failed: %s", err)
+			break
+		}
+		rc := md5.Sum(msg)
+		fmt.Printf("%9d <-- recv %s\n", len(msg), hex.EncodeToString(rc[:]))
+		if md5.Sum(msg) != sc {
+			logrus.Errorf("msg is mismatch")
+		}
+	}
+
+	clientConn.Close()
+}
---
+++ example/udp/server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/ooclab/es/proto/udp"
+)
+
+func main() {
+	addr, err := net.ResolveUDPAddr("udp", os.Args[1])
+	if err != nil {
+		fmt.Println("reslove udp addr failed: ", err)
+		return
+	}
+	// addr := net.UDPAddr{
+	// 	Port: 1234,
+	// 	IP:   net.ParseIP("115.28.213.159"),
+	// }
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+		return
+	}
+	sock, err := udp.NewServerSocket(conn)
+	fmt.Println("sock, err = ", sock, err)
+	defer conn.Close()
+
+	for {
+		conn, err := sock.Accept()
+		if err != nil {
+			fmt.Println("accept failed: ", err)
+			break
+		}
+		fmt.Println("accept client: ", conn)
+		go func() {
+			for {
+				msg, err := conn.RecvMsg()
+				if err != nil {
+					fmt.Printf("recv msg failed: %s\n", err)
+					return
+				}
+				rc := md5.Sum(msg)
+				fmt.Printf("%9d <-- recv %s\n", len(msg), hex.EncodeToString(rc[:]))
+				err = conn.SendMsg(msg)
+				if err != nil {
+					fmt.Println("send msg failed: ", err)
+					return
+				}
+			}
+		}()
+	}
+
+}
